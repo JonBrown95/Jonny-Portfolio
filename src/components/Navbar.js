28497fd1169837a36df508e959e0a6acfd8b64a2
JonBrown95 <126886504+JonBrown95@users.noreply.github.com>
2023-08-03 15:31:53 +0100

    initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+const Navbar = () => {
+    return (
+     <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-3 text-white'>
+        <h1 className="w-full text-3xl font-bold text-[#00df9a]">Jonny Brown</h1>
+        <ul className="flex">
+            <li className="p-4">Contact</li>
+        </ul>
+     </div>
+    )
+}
+
+export default Navbar
