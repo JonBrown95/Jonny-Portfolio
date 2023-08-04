bf87952b1873687579ea46a83bcb5b37eda9305f
JonBrown95 <126886504+JonBrown95@users.noreply.github.com>
2023-08-04 12:06:58 +0100

    Snap-start

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,7 +4,7 @@ const Hero = () => {
     return (
         
     <div className="text-black">
-        <div className="max-w-[800px] mt-[-190px] w-full h-screen mx-auto text-center flex flex-col justify-center">
+        <div className="snap-start max-w-[800px] mt-[-190px] w-full h-screen mx-auto text-center flex flex-col justify-center">
         <h1 className="md:text-6xl sm:text-4xl text-3xl" >Jonathan Brown. </h1>
             <p className="text-[#00df9a] font-bold p-2">Full-Stack developer based in Leeds.</p>
            </div>
