f3092b33e357bd5b0c7b85474d5f0a05d6580105
JonBrown95 <126886504+JonBrown95@users.noreply.github.com>
2023-08-04 12:21:16 +0100

    Hero section now takes up entire screen height

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,16 +1,14 @@
 import React from 'react';
 
 const Hero = () => {
-    return (
-        
-    <div className="text-black">
-        <div className="snap-start max-w-[800px] mt-[-190px] w-full h-screen mx-auto text-center flex flex-col justify-center">
-        <h1 className="md:text-6xl sm:text-4xl text-3xl" >Jonathan Brown. </h1>
-            <p className="text-[#00df9a] font-bold p-2">Full-Stack developer based in Leeds.</p>
-           </div>
-        
+  return (
+    <div className="text-black h-screen bg-white">
+      <div className="max-w-[800px] mx-auto h-full flex flex-col justify-center text-center">
+        <h1 className="md:text-6xl sm:text-4xl text-3xl">Jonathan Brown.</h1>
+        <p className="text-[#00df9a] font-bold p-2">Full-Stack developer based in Leeds.</p>
+      </div>
     </div>
-    )
+  );
 }
 
-export default Hero
+export default Hero;
