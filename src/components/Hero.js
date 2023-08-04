6153219edcba65adde386a2dbca2ce6ca8df12bb
JonBrown95 <126886504+JonBrown95@users.noreply.github.com>
2023-08-04 12:33:15 +0100

    Moved text up

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Hero = () => {
   return (
-    <div className="text-black h-screen bg-white">
+    <div className="text-black h-screen bg-white px-6">
       <div className="max-w-[800px] mx-auto h-full flex flex-col justify-center text-center">
-        <h1 className="md:text-6xl sm:text-4xl text-3xl">Jonathan Brown.</h1>
+        <h1 className="md:text-6xl sm:text-4xl text-3xl mt-[-300px]">Jonathan Brown.</h1>
         <p className="text-[#00df9a] font-bold p-2">Full-Stack developer based in Leeds.</p>
       </div>
     </div>
