b8f12d2025f7f0da6ab511bcdb3a9711bdabf460
JonBrown95 <126886504+JonBrown95@users.noreply.github.com>
2023-08-06 13:21:09 +0100

    Styled navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import blackLogo from '../images/blackLogo.svg'
 const Navbar = () => {
     return (
         <nav className="sticky top-0 z-10 backdrop-filter backdrop-blur-lg bg-opacity-0 bg-white">
-  <div className="max-w-5xl mx-auto px-4">
+  <div className="w-full mx-auto px-4 fixed top-0 z-50 backdrop-blur-xl">
     <div className="flex items-center justify-between h-16">
     <img src={blackLogo} alt="Logo for Jonny's portfolio - initials JB in <> tags" className="h-14 w-14" />
       <div className="flex space-x-4 text-gray-900">
