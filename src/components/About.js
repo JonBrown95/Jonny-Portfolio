246811f230d2422ecae29f41f50acca08813b92e
JonBrown95 <126886504+JonBrown95@users.noreply.github.com>
2023-08-06 13:15:25 +0100

    Added scroll down button on Hero page

--- src/components/About.js
+++ src/components/About.js
@@ -5,10 +5,11 @@ const About = () => {
         <div className="w-full h-[80vh] short:h-full max-w-[900px] p-10 px-10 md:p-10 max-h-[600px] ">
         <h1 className="font-bold mb-2 md:mb-5 text-3xl md:text-4xl lg:text-6xl">About</h1>
         <hr className="border-[#ffffff] border-[4px] rounded w-12 mb-4" />
-              <div className="space-y-2 sm:space-y-3 text-[12px] md:space-y-5 sm:my-3 sm:text-sm lg:text-[18px] lg:leading-7 dark:text-stone-900">
-                <p className='p-2 bg-white rounded-md'> 
+              <div className="space-y-2 sm:space-y-3 text-[12px] md:space-y-5 sm:my-3 sm:text-sm lg:text-[16px] lg:leading-7 dark:text-stone-900">
+                <p className='p-3 bg-white rounded-md'> 
                   I pride myself in being hard-working, a people person and as someone who will always go the extra mile. I made the decision to retrain as a <b>software developer</b> and have not regretted it. The problem solving, the fact that the sky is the limit when it comes to coding and how you only get out what you put in is something I love.
-                </p>
+</p>
+<p className="p-3 bg-white rounded-md">Hello mate</p>
                 
               </div>
         </div>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,17 +1,22 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 const Hero = () => {
 
+
   return (
     <div className="text-black h-screen w-screen bg-white" id="hero">
-      <div className="snap-start max-w-[800px] mx-auto h-full flex flex-col justify-center text-center">
+      <div className="relative snap-start w-screen mx-auto h-full flex flex-col justify-center text-center">
         <h1 className="md:text-5xl sm:text-4xl text-3xl mt-[-350px]">Hello, my name is Jonny!</h1>
         <p className="text-[#00df9a] font-bold p-2">Full-Stack developer based in Leeds.</p>
-        
-        
-      </div>
+
+        <div class="absolute flex flex-col gap-1 text-black animate-bounce w-8 h-8 bottom-4 right-2 md:right-4">
+        <a class="flex items-center justify-center" href="#about">
+        <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" class="w-5 h-5 cursor-pointer" height="25" width="25" xmlns="http://www.w3.org/2000/svg"><circle cx="12" cy="12" r="10"></circle><polyline points="8 12 12 16 16 12"></polyline><line x1="12" y1="8" x2="12" y2="16"></line></svg>
+       </a></div>
+        </div>
     </div>
   );
 };
 
 export default Hero;
+
