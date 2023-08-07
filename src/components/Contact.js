77cdf7918e422678d43f9a58b039ccb7db411477
JonBrown95 <126886504+JonBrown95@users.noreply.github.com>
2023-08-07 17:32:34 +0100

    Changed font to Mukta

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,15 +1,22 @@
 const Contact = () => {
   return (
-    <div
+    <footer
       id="contact"
-      class="snap-end bg-opacity-90 backdrop-blur-xl p-10 text-xs md:text-sm bg-stone-100 flex flex-col gap-5 text-stone-600 dark:text-stone-400 justify-center items-center w-screen h-[50dvh] min-h-[300px] max-h-[600px]"
+      class="snap-end bg-opacity-40 p-10 text-xs md:text-sm bg-slate-300 flex flex-col gap-5 text-black gap-4 justify-center items-center w-screen h-[300dvh] min-h-[100px] max-h-[200px] font-bold subpixel-antialiased "
     >
-      <div className="text-center max-w-[300px]  text-black ">
-        <p>Jonathan Brown</p>
-        <p>www.jonathanbrown.me |</p>
+      <div className="text-center md:max-w-[500px] max-w-[300px]    ">
+        <p>Created by Jonathan Brown using React with Tailwind CSS </p>
         
       </div>
-    </div>
+
+      <div className="text-center md:max-w-[500px] max-w-[300px]     ">
+        <p>Whats up |</p>
+      </div>
+
+      <div className="text-center md:max-w-[500px] max-w-[300px]   underline   ">
+        <p>www.google.com</p>
+      </div>
+    </footer>
   );
 };
 
