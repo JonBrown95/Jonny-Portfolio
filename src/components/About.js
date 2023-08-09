9a3b53d4daad5af686775a8ca1ea420cf84e5f72
JonBrown95 <126886504+JonBrown95@users.noreply.github.com>
2023-08-09 14:48:26 +0100

    Restyled about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Transition } from '@headlessui/react';
 import { InView } from 'react-intersection-observer';
+import JavaScriptIcon from '../images/Javascript.svg';
+
 
 const About = () => {
   const [animationTriggered, setAnimationTriggered] = useState(false);
@@ -12,8 +14,8 @@ const About = () => {
   };
 
   return (
-    <div id="about" className="snap-start bg-[#00df9a] w-screen h-screen flex items-center justify-center text-8xl">
-      <div className="w-full h-[80vh] short:h-full max-w-[900px] p-10 px-10 md:p-10 max-h-[600px]">
+    <div id="about" className="snap-start bg-[#00df9a] w-screen h-screen flex items-center justify-center text-8xl" >
+      <div className="w-full h-[80vh] short:h-full max-w-[900px] mt-[-50px] p-10 px-10 md:p-10 max-h-[600px]">
         <InView onChange={handleInView}>
           {({ inView, ref }) => (
             <div ref={ref}>
@@ -23,16 +25,16 @@ const About = () => {
                   enter="ease-in duration-[1000ms] transition-all"
                   enterFrom="opacity-0 translate-y-12"
                   enterTo="opacity-100 translate-y-0"
-                  className="font-bold mb-2 md:mb-5 text-3xl md:text-4xl lg:text-6xl"
+                  className="flex justify-center font-bold mb-2 md:mb-5 text-3xl md:text-4xl lg:text-6xl"
                 >
-                  About
+                  About Me
                 </Transition.Child>
                 <Transition.Child
                   as="hr"
                   enter="ease-in duration-[1000ms] delay-[200ms] transition-all"
                   enterFrom="opacity-0 translate-y-12"
                   enterTo="opacity-100 translate-y-0"
-                  className="border-[#ffffff] border-[4px] rounded w-12 mb-4"
+                  className="flex justify-center border-[#ffffff] border-[2px] rounded w-13 mb-4"
                 />
                 <Transition.Child
                   as="div"
@@ -53,6 +55,32 @@ const About = () => {
                     problem solving, the fact that the sky is the limit when it comes to coding and how you only get
                     out what you put in is something I love.
                   </p>
+                </Transition.Child>
+
+                
+                <Transition.Child
+                  as="div"
+                  enter="ease-in duration-[1000ms] delay-[400ms] transition-all"
+                  enterFrom="opacity-0 translate-y-12"
+                  enterTo="opacity-100 translate-y-0"
+                  className="mt-8"
+                >
+                  <h1 className="flex justify-center font-bold text-3xl md:text-4xl lg:text-6xl">
+                    Skills
+                  </h1>
+                  <hr className="border-[#ffffff] border-[2px] rounded w-13 mx-auto mt-2" />
+                  <div className="flex justify-center gap-6 mt-4">
+                    <div className="w-16 h-16  rounded-lg shadow-lg flex items-center justify-center cursor-pointer transform hover:scale-110 transition-transform">
+                      <img src={JavaScriptIcon} alt="JavaScript Icon" />
+                    </div>
+                    <div className="w-16 h-16 bg-white rounded-lg shadow-lg flex items-center justify-center cursor-pointer transform hover:rotate-12 transition-transform">
+
+
+
+                      <img src={JavaScriptIcon} alt="MySQL Icon" />
+                    </div>
+                    {/* add icons here */}
+                  </div>
                 </Transition.Child>
               </Transition.Root>
             </div>
