69019ff479ac3c6ba04c6a63b87a998274f2c6be
JonBrown95 <126886504+JonBrown95@users.noreply.github.com>
2023-08-30 21:48:16 +0100

    Styled and completed contact section - added email and phone number.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,20 +1,24 @@
+import React from "react";
+import { FiPhone, FiMail, } from "react-icons/fi";
+
 const Contact = () => {
   return (
     <footer
       id="contact"
-      class="snap-end bg-opacity-40 p-10 text-xs md:text-sm bg-slate-300 flex flex-col gap-5 text-black gap-4 justify-center items-center w-screen h-[300dvh] min-h-[100px] max-h-[200px] font-bold subpixel-antialiased "
+      className="snap-end bg-opacity-40 p-10 text-xs md:text-sm bg-slate-300 flex flex-col gap-5 text-black gap-4 justify-center items-center w-screen h-[300dvh] min-h-[100px] max-h-[200px] font-bold subpixel-antialiased"
     >
-      <div className="text-center md:max-w-[500px] max-w-[300px]    ">
-        <p>Created by Jonathan Brown using React with Tailwind CSS </p>
-        
+      <div className="text-center md:max-w-[500px] max-w-[300px]">
+        <p>Created by Jonathan Brown using React with Tailwind CSS</p>
       </div>
 
-      <div className="text-center md:max-w-[500px] max-w-[300px]     ">
-        <p>Whats up |</p>
-      </div>
-
-      <div className="text-center md:max-w-[500px] max-w-[300px]   underline   ">
-        <p>www.google.com</p>
+      <div className="text-center md:max-w-[500px] max-w-[300px]">
+        <p className="mb-1">For inquiries, feel free to reach out:</p>
+        <p>
+          <FiPhone className="inline mr-1 text-[#00df9a]" />{" "}
+          <a href="tel:+447803199773">07803 199 773</a> 
+          <FiMail className="inline mx-1 text-[#00df9a]" />{" "}
+          <a href="mailto:brownjon41@gmail.com">brownjon41@gmail.com</a>
+        </p>
       </div>
     </footer>
   );
